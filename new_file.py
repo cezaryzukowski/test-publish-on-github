bd5aa39604b55228e132e4110afa4ce0ce7219c4
Cezary Zukowski <cezary.zukowski@gmail.com>
2016-11-23 22:43:46 +0100

    A few new files were added to examine: 1) "Refactoring -> Rename" In a situation when a filename of the file which is referenced in other file gets changed. 2) Does "Ctrl+Alt+A" adds a single file or entire group a the files to VCS? A: single

---
+++ new_file.py
@@ -0,0 +1,4 @@
+
+# A change introduced after the file was added.
+# Color has not changed to "blue". So, an uncommitted file is never marked as "blue" (modified).
+# First, a file need to be committed in order to enable "blue" indicator attribute for the file.
